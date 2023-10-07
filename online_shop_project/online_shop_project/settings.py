6812229ac0da8b80ed9e97759cd90795916914de
Mahdi Abbasi <77788727+MahdiAbbasi7@users.noreply.github.com>
2023-10-07 19:48:41 +0330

    Update settings.py

--- online_shop_project/online_shop_project/settings.py
+++ online_shop_project/online_shop_project/settings.py
@@ -87,6 +87,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates']
+        # todo : change to 'DIRS': [str(BASE_DIR.joinpath('templates'))]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
