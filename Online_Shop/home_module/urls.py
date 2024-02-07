8dc7217f93df38092054887277f674ae9a5593e7
Mahdi Abbasi <abbasimahdi782@gmail.com>
2024-02-07 11:05:18 +0330

    Reset all files

--- Online_Shop/home_module/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.IndexView.as_view(), name="index_page"),
-    path('about-us',views.AboutView.as_view(), name="about_us")
-    # path('contact-us', views.contact_page),
-    # path('site-header', views.site_header_component,)
-]
